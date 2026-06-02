a404a11a941a9235e482c1a82d0459bdf26e95e6
stefhany <batigute@gmail.com>
2026-06-02 08:11:58 -0300

    Fazendo atividade 5

---
+++ curso_irede/src/lista_02/at6/ProdutoAlimenticio.java
@@ -0,0 +1,24 @@
+package lista_02.at6;
+
+import java.time.LocalDate;
+
+public class ProdutoAlimenticio extends Produto {
+	private LocalDate dataValidade;
+	public ProdutoAlimenticio(String nome, Integer quantity, Double valor, LocalDate dataValidade) {
+		super(nome, quantity, valor);
+		this.dataValidade = dataValidade;
+	}
+	public LocalDate getDataValidade() {
+		return dataValidade;
+	}
+	public boolean isVencido() {
+		LocalDate dataHoje = LocalDate.now();
+		if (dataHoje.isAfter(dataValidade)) {
+			return true;
+		} 
+		return false;
+	}
+	
+	
+	
+}
